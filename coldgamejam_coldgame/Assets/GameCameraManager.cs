fca87b857c358b458ab72ead3f3d06f3ca6694fc
minhoKim <flypizza2@gmail.com>
2020-02-06 23:02:57 +0900

    i did not anything

---
+++ coldgamejam_coldgame/Assets/GameCameraManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Com.LuisPedroFonseca.ProCamera2D;
+
+public class GameCameraManager : MonoBehaviour
+{
+    public ProCamera2DCinematics proCamera2DCinematics;
+
+    public void Awake()
+    {
+        proCamera2DCinematics = FindObjectOfType<ProCamera2DCinematics>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        proCamera2DCinematics.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ coldgamejam_coldgame/Assets/Scenes/IntroScene/IntroTitleText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+public class IntroTitleText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 cur_pos = transform.position;
+        RectTransform rt = GetComponent<RectTransform>();
+
+        Sequence seq = DOTween.Sequence();
+        seq.Append(rt.DOAnchorPosY(1.0f, 0.1f).SetDelay(0.3f));
+        seq.Append(rt.DOShakeAnchorPos(0.3f));
+        seq.AppendCallback(CallBack_test);
+
+
+    }
+
+    public void CallBack_test()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
